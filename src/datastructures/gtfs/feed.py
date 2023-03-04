59215fb860c657c0580fab25b7d1f6b4a312a7be
Julius Heinzinger <julius.heinzinger@gmail.com>
2023-03-04 11:22:48 +0100

    Implement feed creation

--- src/datastructures/gtfs/feed.py
+++ src/datastructures/gtfs/feed.py
@@ -1,6 +1,11 @@
 from io import StringIO
 
+from src.datastructures.gtfs.calendar import Calendar
+from src.datastructures.gtfs.calendar_dates import CalendarDate
+from src.datastructures.gtfs.routes import Route
 from src.datastructures.gtfs.stop import Stop
+from src.datastructures.gtfs.stop_times import StopTime
+from src.datastructures.gtfs.trips import Trip
 
 
 class Feed:
@@ -10,3 +15,10 @@ class Feed:
 
     def _create(self, data: dict[str: StringIO]) -> None:
         self.stops = Stop.from_buffer(data["stops"])
+        self.routes = Route.from_buffer(data["routes"])
+        self.trips = Trip.from_buffer(data["trips"])
+        self.stop_times = StopTime.from_buffer(data["stop_times"])
+        buf = data.get("calendar")
+        self.calendar = Calendar.from_buffer(buf) if buf else None
+        buf = data.get("calendar_dates")
+        self.calendar_dates = CalendarDate.from_buffer(buf) if buf else None
